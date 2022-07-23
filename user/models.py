e649dc406f0a0e6b8427d2e144c77c8ae55cfe8a
expensive77 <51305499+expensive77@users.noreply.github.com>
2022-07-23 23:32:29 +0430

    add CustomUser model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    name = models.CharField(max_length=300)
+    username = models.CharField(max_length=300, unique=True)
+    email = models.CharField(max_length=400, unique=True)
+    password = models.CharField(max_length=100)
+    phone_number = models.IntegerField(default=0 , null=True)
+    picture=models.ImageField(null=True, blank=True , upload_to='media/profiles/', default='media/profiles/default_pp.png')
+    address = models.TextField()
+    def __str__(self):
+        return self.name
