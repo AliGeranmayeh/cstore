c547a7cf8dd1e371ff6d88fb5f46b2adaf33556b
expensive77 <51305499+expensive77@users.noreply.github.com>
2022-07-23 23:59:36 +0430

    update picture path

--- user/models.py
+++ user/models.py
@@ -8,7 +8,7 @@ class CustomUser(AbstractUser):
     email = models.CharField(max_length=400, unique=True)
     password = models.CharField(max_length=100)
     phone_number = models.IntegerField(default=0 , null=True)
-    picture=models.ImageField(null=True, blank=True , upload_to='media/profiles/', default='media/profiles/default_pp.png')
+    picture=models.ImageField(null=True, blank=True , upload_to='media/profiles/', default='profiles/default_pp.png')
     address = models.TextField()
     def __str__(self):
         return self.name
